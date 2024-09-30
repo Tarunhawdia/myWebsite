19f4cd59c6158b30bc15985dc31fc0bcb8ebe198
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-09-30 18:27:36 +0530

    made intial outline

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="navbar">
+          <h1>Tarun Hawdia</h1>
+        </div>
+        <div className="inside-container">
+          <div className="sidebar">
+            <h1>about me</h1>
+            <h1>projects</h1>
+          </div>
+          <div className="main_content">
+            <h1>main content</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              mollitia quidem adipisci voluptate laudantium impedit non fuga
+              reiciendis rem recusandae omnisn repellat temporibus explicabo
+              nobis sed perspiciatis excepturi earum, nam dolor facilis
+              voluptatem dicta doloribus deleniti. Ullam nostrum, reiciendis
+              aliquid labore illum soluta dignissimos animi laborum repellendus
+              magni dolores quia? Sed accusamus nulla optio vitae quo laboriosam
+              animi aperiam dolore aliquid ipsum. Voluptatibus dolorem placeat
+              ullam eius asperiores, blanditiis sapiente nostrum modi minus
+              rerum rem fuga porro assumenda tempora repudiandae, harum vel
+              enim! Vitae nesciunt iure, harum ab vero aperiam commodi
+              perspiciatis obcaecati? Aliquam dicta consequuntur fugit tempora
+              aspernatur ratione.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
