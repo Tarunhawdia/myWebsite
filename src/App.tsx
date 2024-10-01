c4e41a0dcc49d8f72dd313556050a4807ca40067
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-10-01 11:45:30 +0530

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import pic from "./assets/pic.jpg";
+import logo from "./assets/logo_stc.png";
 
 function App() {
   return (
     <>
       <div className="main-container">
         <div className="navbar">
-          <h1>Tarun Hawdia</h1>
+          <h1 className="Tarun_Hawdia">Tarun Hawdia</h1>
         </div>
         <div className="inside-container">
           <div className="sidebar">
@@ -18,42 +20,133 @@ function App() {
               </li>
             </ul>
           </div>
-
           <div className="section_main">
             <section id="about" className="section">
-              <h2>About Me</h2>
+              <img src={pic} alt="tarun hawdia profile pic" loading="lazy" />
+              <h1 className="sub_headings">About Me</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea odit
-                ullam, dolores ex delectus non quia voluptatem possimus, eaque
-                eius sapiente laudantium? Veniam hic labore, voluptatibus
-                similique perferendis magni tempore doloremque, nisi beatae iste
-                alias. Non ratione numquam deserunt exercitationem iusto
-                incidunt fuga nostrum id dolorem officia placeat, error quia sed
-                quae necessitatibus dicta aliquid ea nobis. Laborum enim itaque
-                molestias quis iure temporibus eaque rem soluta, ad odio nihil,
-                sunt repudiandae provident vel! Expedita mollitia vitae ipsum
-                optio explicabo ex quos porro illo, atque sunt reprehenderit
-                libero placeat vero vel voluptates modi, velit nam culpa
-                consequuntur nulla. Libero, deleniti.
+                Hi! I am Tarun , a sotware developer wokring at ArthmateTech
+                Pvt. Ltd.
               </p>
             </section>
 
             <section id="projects" className="section">
-              <h2>Projects</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea odit
-                ullam, dolores ex delectus non quia voluptatem possimus, eaque
-                eius sapiente laudantium? Veniam hic labore, voluptatibus
-                similique perferendis magni tempore doloremque, nisi beatae iste
-                alias. Non ratione numquam deserunt exercitationem iusto
-                incidunt fuga nostrum id dolorem officia placeat, error quia sed
-                quae necessitatibus dicta aliquid ea nobis. Laborum enim itaque
-                molestias quis iure temporibus eaque rem soluta, ad odio nihil,
-                sunt repudiandae provident vel! Expedita mollitia vitae ipsum
-                optio explicabo ex quos porro illo, atque sunt reprehenderit
-                libero placeat vero vel voluptates modi, velit nam culpa
-                consequuntur nulla. Libero, deleniti.
-              </p>
+              <h1 className="sub_headings">Projects</h1>
+              <div className="projects_container">
+                <div className="project_tile">
+                  <a
+                    href="https://github.com/Tarunhawdia/myChessApp"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      display: "flex",
+                      textDecoration: "none",
+                      color: "inherit",
+                    }}
+                  >
+                    <img
+                      style={{ height: "100px", width: "300px" }}
+                      src={logo}
+                      alt="Project 1 Logo"
+                      className="project-logo"
+                    />
+                  </a>
+                  <div className="project_info">
+                    <a
+                      href="https://github.com/Tarunhawdia/myChessApp"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                      <p className="project_title">Project 1</p>
+                    </a>
+                    <p className="project_description">
+                      A brief description of Project 1. Lorem ipsum dolor sit
+                      amet, consectetur adipisicing elit Lorem ipsum, dolor sit
+                      amet consectetur adipisicing elit. Iusto, magni sint!
+                      Incidunt assumenda quo culpa, perferendis eveniet iure
+                      magni quis laboriosam delectus corrupti consequatur porro
+                      iste sit voluptas tempore nihil.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="project_tile">
+                  <a
+                    href="https://github.com/Tarunhawdia/myChessApp"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      display: "flex",
+                      textDecoration: "none",
+                      color: "inherit",
+                    }}
+                  >
+                    <img
+                      style={{ height: "100px", width: "300px" }}
+                      src={logo}
+                      alt="Project 1 Logo"
+                      className="project-logo"
+                    />
+                  </a>
+                  <div className="project_info">
+                    <a
+                      href="https://github.com/Tarunhawdia/myChessApp"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                      <p className="project_title">Project 1</p>
+                    </a>
+                    <p className="project_description">
+                      A brief description of Project 1. Lorem ipsum dolor sit
+                      amet, consectetur adipisicing elit Lorem ipsum, dolor sit
+                      amet consectetur adipisicing elit. Iusto, magni sint!
+                      Incidunt assumenda quo culpa, perferendis eveniet iure
+                      magni quis laboriosam delectus corrupti consequatur porro
+                      iste sit voluptas tempore nihil.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="project_tile">
+                  <a
+                    href="https://github.com/Tarunhawdia/myChessApp"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      display: "flex",
+                      textDecoration: "none",
+                      color: "inherit",
+                    }}
+                  >
+                    <img
+                      style={{ height: "100px", width: "300px" }}
+                      src={logo}
+                      alt="Project 1 Logo"
+                      className="project-logo"
+                    />
+                  </a>
+                  <div className="project_info">
+                    <a
+                      href="https://github.com/Tarunhawdia/myChessApp"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                      <p className="project_title">Project 1</p>
+                    </a>
+                    <p className="project_description">
+                      A brief description of Project 1. Lorem ipsum dolor sit
+                      amet, consectetur adipisicing elit Lorem ipsum, dolor sit
+                      amet consectetur adipisicing elit. Iusto, magni sint!
+                      Incidunt assumenda quo culpa, perferendis eveniet iure
+                      magni quis laboriosam delectus corrupti consequatur porro
+                      iste sit voluptas tempore nihil.
+                    </p>
+                  </div>
+                </div>
+              </div>
             </section>
           </div>
         </div>
