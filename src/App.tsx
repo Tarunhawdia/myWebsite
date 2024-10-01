7f419ae1e0071fa900aac118ae538a00a6239b5a
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-10-01 12:37:02 +0530

    correted UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,29 @@ function App() {
       <div className="main-container">
         <div className="navbar">
           <h1 className="Tarun_Hawdia">Tarun Hawdia</h1>
+          <div className="social-links">
+            <a
+              href="https://github.com/Tarunhawdia"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://linkedin.com/in/tarunhawdia"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://x.com/HawdiaTarun"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </div>
         </div>
         <div className="inside-container">
           <div className="sidebar">
