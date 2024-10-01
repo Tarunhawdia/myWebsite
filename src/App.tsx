386ff799c2417230ba8b443343bf663a925f0247
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-10-01 07:45:57 +0530

    corrected some style

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,52 @@ function App() {
         </div>
         <div className="inside-container">
           <div className="sidebar">
-            <h1>about me</h1>
-            <h1>projects</h1>
+            <ul>
+              <li>
+                <a href="#about">About Me</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+            </ul>
           </div>
-          <div className="main_content">
-            <h1>main content</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-              mollitia quidem adipisci voluptate laudantium impedit non fuga
-              reiciendis rem recusandae omnisn repellat temporibus explicabo
-              nobis sed perspiciatis excepturi earum, nam dolor facilis
-              voluptatem dicta doloribus deleniti. Ullam nostrum, reiciendis
-              aliquid labore illum soluta dignissimos animi laborum repellendus
-              magni dolores quia? Sed accusamus nulla optio vitae quo laboriosam
-              animi aperiam dolore aliquid ipsum. Voluptatibus dolorem placeat
-              ullam eius asperiores, blanditiis sapiente nostrum modi minus
-              rerum rem fuga porro assumenda tempora repudiandae, harum vel
-              enim! Vitae nesciunt iure, harum ab vero aperiam commodi
-              perspiciatis obcaecati? Aliquam dicta consequuntur fugit tempora
-              aspernatur ratione.
-            </p>
+
+          <div className="section_main">
+            <section id="about" className="section">
+              <h2>About Me</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea odit
+                ullam, dolores ex delectus non quia voluptatem possimus, eaque
+                eius sapiente laudantium? Veniam hic labore, voluptatibus
+                similique perferendis magni tempore doloremque, nisi beatae iste
+                alias. Non ratione numquam deserunt exercitationem iusto
+                incidunt fuga nostrum id dolorem officia placeat, error quia sed
+                quae necessitatibus dicta aliquid ea nobis. Laborum enim itaque
+                molestias quis iure temporibus eaque rem soluta, ad odio nihil,
+                sunt repudiandae provident vel! Expedita mollitia vitae ipsum
+                optio explicabo ex quos porro illo, atque sunt reprehenderit
+                libero placeat vero vel voluptates modi, velit nam culpa
+                consequuntur nulla. Libero, deleniti.
+              </p>
+            </section>
+
+            <section id="projects" className="section">
+              <h2>Projects</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea odit
+                ullam, dolores ex delectus non quia voluptatem possimus, eaque
+                eius sapiente laudantium? Veniam hic labore, voluptatibus
+                similique perferendis magni tempore doloremque, nisi beatae iste
+                alias. Non ratione numquam deserunt exercitationem iusto
+                incidunt fuga nostrum id dolorem officia placeat, error quia sed
+                quae necessitatibus dicta aliquid ea nobis. Laborum enim itaque
+                molestias quis iure temporibus eaque rem soluta, ad odio nihil,
+                sunt repudiandae provident vel! Expedita mollitia vitae ipsum
+                optio explicabo ex quos porro illo, atque sunt reprehenderit
+                libero placeat vero vel voluptates modi, velit nam culpa
+                consequuntur nulla. Libero, deleniti.
+              </p>
+            </section>
           </div>
         </div>
       </div>
