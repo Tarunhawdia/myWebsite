fec2daf14f68d5ab85a98d6b362172b875127f19
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-10-03 18:22:50 +0530

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import BackgroundVideo from './subHeadings/BackgroundVideo.tsx'
+// import BackgroundVideo from './subHeadings/BackgroundVideo.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BackgroundVideo/>
+    {/* <BackgroundVideo/> */}
     <App />
   </StrictMode>,
 )
