f7af1fc43d309fe11d279f42475507967d00b9dc
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-10-07 20:19:46 +0530

    corrected mistakes

--- src/subHeadings/Experience.tsx
+++ src/subHeadings/Experience.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Experience.css'
+import "./Experience.css";
 
 const Experience: React.FC = () => {
   const experiences = [
@@ -15,12 +15,23 @@ const Experience: React.FC = () => {
       ],
     },
     {
+      company: "Great India Bathroom Fittings",
+      title: "Developer",
+      period: "Mar 2021 - Apr 2022",
+      description: [
+        "Worked on Android app on the play store that displays the latest prices of metals such as Zinc, Brass, etc",
+        "Enhanced user experience by developing responsive and dynamic UIs with React.Successfully integrated Google AdMob to monetize the app with targeted advertisements",
+        "Worked on microservices architecture and implemented REST APIs.",
+        "Collaborated with front-end developers to integrate with React components.Developed the app using React Native for the frontend, Express.js for the Backend and MongoDB as NOSQL DB",
+      ],
+    },
+    {
       company: "Platfourma",
       title: "Project Trainee and Intern",
       period: "May 2018 - July 2018",
       description: [
         "Assisted in the development of full-stack web applications.       Designed and programmed a line-following robot using Arduino Uno, demonstrating embedded C expertise.",
-        "I have build several project one of them was an obstacle-avoiding robot with Arduino Uno and infrared sensors, programmed in C."
+        "I have build several project one of them was an obstacle-avoiding robot with Arduino Uno and infrared sensors, programmed in C.",
       ],
     },
   ];
