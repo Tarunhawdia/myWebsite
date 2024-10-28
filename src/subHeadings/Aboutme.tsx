9efe74952a8b4bfa3152177f8ced7add497f7fc7
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-10-28 21:48:03 +0530

    edited about me

--- src/subHeadings/Aboutme.tsx
+++ src/subHeadings/Aboutme.tsx
@@ -7,42 +7,37 @@ const AboutMe: React.FC = () => {
       <img src={pic} alt="tarun hawdia profile pic" loading="lazy" />
       <h1 className="sub_headings">About Me</h1>
       <p>
-        Hi! I am Tarun, a software developer working at ArthmateTech Pvt. Ltd.
+        Hi! I am Tarun, a Software Engineer at ArthmateTech Pvt. Ltd. with a
+        strong academic foundation in Biomedical Science and Electrical
+        Engineering, holding an M.Tech from IIT Guwahati and a B.Tech from
+        Ujjain Engineering College.
       </p>
+
       <p>
-        I am a Software Engineer with a strong academic foundation in Biomedical
-        Science and Electrical Engineering, holding an M.Tech from IIT Guwahati
-        and a B.Tech from Ujjain Engineering College.
+        Although I don't hold a CS degree, I’ve gained extensive knowledge
+        through YouTube, online courses, and blogs. At Arthmatetech, I enhance
+        backend systems with Node.js and Java, while developing frontend
+        applications using ReactJS. My expertise spans web development, machine
+        learning, and automation testing.
       </p>
+
       <p>
-        Although I do not have a CS degree, I am obsessed with computer science.
-        I got most of my knowledge by watching YouTube videos, taking online
-        courses, and reading lots of blogs.
+        Previously, I developed an Android app for Great India Bathroom Fittings
+        using React Native, Express.js, and MongoDB, integrating Google AdMob
+        for monetization.
       </p>
+
       <p>
-        Currently, I am working at Arthmatetech Pvt. Ltd., where I enhance
-        backend systems using Node.js and Java, and develop frontend
-        applications with ReactJS . My technical expertise spans web
-        development, machine learning, and automation testing.
+        I’m passionate about problem-solving, having solved 500+ DSA questions
+        on platforms like LeetCode and Codeforces. Additionally, I enjoy
+        embedded systems, demonstrated by my internship designing Arduino-based
+        robots.
       </p>
+
       <p>
-        Previously, I worked at Great India Bathroom Fittings, where I developed
-        an Android app using React Native, Express.js, and MongoDB, and
-        integrated Google AdMob for monetization.
-      </p>
-      <p>
-        Additionally, I have a deep interest in embedded systems, demonstrated
-        by my internship, where I designed robots using Arduino.
-        <p>
-          I am passionate about problem-solving, having solved 500+ DSA
-          questions on competitive platforms like LeetCode and Codeforces.
-        </p>
-      </p>
-      <p>
-        I am also an enthusiastic chess player, winning a gold medal in an
-        inter-hostel competition at IITG. With a strong technical skillset and a
-        constant drive to learn, I enjoy tackling challenges and building
-        efficient, scalable systems.
+        A chess enthusiast, I won a gold medal at IIT Guwahati. With a strong
+        technical skill set and constant drive to learn, I enjoy tackling
+        challenges and building scalable systems.
       </p>
     </section>
   );
