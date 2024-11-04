fb9d90d61520b8cd373becc9923a0a088b73280c
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-11-04 19:44:25 +0530

    corrected about me

--- src/subHeadings/Aboutme.tsx
+++ src/subHeadings/Aboutme.tsx
@@ -7,37 +7,22 @@ const AboutMe: React.FC = () => {
       <img src={pic} alt="tarun hawdia profile pic" loading="lazy" />
       <h1 className="sub_headings">About Me</h1>
       <p>
-        Hi! I am Tarun, a Software Engineer at ArthmateTech Pvt. Ltd. with a
-        strong academic foundation in Biomedical Science and Electrical
-        Engineering, holding an M.Tech from IIT Guwahati and a B.Tech from
-        Ujjain Engineering College.
+        Hi! I’m <strong>Tarun Hawdia</strong>, a Software Developer with 1.6
+        years of experience and an academic background in Biomedical Science and
+        Electrical Engineering (M.Tech from IIT Guwahati). Skilled in backend
+        (Node.js, Java) and frontend (ReactJS), I enhance systems and develop
+        applications.
       </p>
-
       <p>
-        Although I don't hold a CS degree, I’ve gained extensive knowledge
-        through YouTube, online courses, and blogs. At Arthmatetech, I enhance
-        backend systems with Node.js and Java, while developing frontend
-        applications using ReactJS. My expertise spans web development, machine
-        learning, and automation testing.
+        At <strong>ArthmateTech</strong>, I focused on scalable backend
+        solutions using Node.js, React, and MongoDB. Previously, I built an
+        Android app with React Native and Express.js for Great India Bathroom
+        Fittings, integrating Google AdMob for monetization.
       </p>
-
       <p>
-        Previously, I developed an Android app for Great India Bathroom Fittings
-        using React Native, Express.js, and MongoDB, integrating Google AdMob
-        for monetization.
-      </p>
-
-      <p>
-        I’m passionate about problem-solving, having solved 500+ DSA questions
-        on platforms like LeetCode and Codeforces. Additionally, I enjoy
-        embedded systems, demonstrated by my internship designing Arduino-based
-        robots.
-      </p>
-
-      <p>
-        A chess enthusiast, I won a gold medal at IIT Guwahati. With a strong
-        technical skill set and constant drive to learn, I enjoy tackling
-        challenges and building scalable systems.
+        Passionate about problem-solving, I’ve solved 500+ DSA questions and
+        enjoy embedded systems and robotics. I’m also a chess enthusiast and IIT
+        Guwahati gold medalist.
       </p>
     </section>
   );
