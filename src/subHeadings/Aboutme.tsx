04195849a4e0acecf5489d466c606d41da206e6e
Tarunhawdia <tarunhawdiya112@gmail.com>
2025-03-27 00:56:40 +0530

    corrected about me

--- src/subHeadings/Aboutme.tsx
+++ src/subHeadings/Aboutme.tsx
@@ -7,22 +7,35 @@ const AboutMe: React.FC = () => {
       <img src={pic} alt="tarun hawdia profile pic" loading="lazy" />
       <h1 className="sub_headings">About Me</h1>
       <p>
-        Hi! I’m <strong>Tarun Hawdia</strong>, a Software Developer with 1.6
-        years of experience and an academic background in Biomedical Science and
-        Electrical Engineering (M.Tech from IIT Guwahati). Skilled in backend
-        (Node.js, Java) and frontend (ReactJS), I enhance systems and develop
-        applications.
+        Hi! I’m <strong>Tarun Hawdia</strong>, a Software Developer with{" "}
+        <strong>1.6 years of experience</strong> and an academic background in{" "}
+        <strong>Biomedical Science and Electrical Engineering</strong> (M.Tech
+        from IIT Guwahati). Skilled in backend development (
+        <strong>Java, Node.js</strong>) and frontend (<strong>ReactJS</strong>),
+        I specialize in building scalable and efficient systems.
       </p>
+
       <p>
-        At <strong>ArthmateTech</strong>, I focused on scalable backend
-        solutions using Node.js, React, and MongoDB. Previously, I built an
-        Android app with React Native and Express.js for Great India Bathroom
-        Fittings, integrating Google AdMob for monetization.
+        At <strong>ArthmateTech</strong>, I developed and optimized backend
+        solutions using <strong>Java Spring Boot, Node.js</strong>, and{" "}
+        <strong>MongoDB</strong>, contributing to loan processing applications
+        with a focus on performance and reliability. I also implemented{" "}
+        <strong>RESTful APIs</strong>, integrated third-party services, and
+        ensured system stability through rigorous testing.
       </p>
+
       <p>
-        Passionate about problem-solving, I’ve solved 500+ DSA questions and
-        enjoy embedded systems and robotics. I’m also a chess enthusiast and IIT
-        Guwahati gold medalist.
+        Previously, at <strong>Great India Bathroom Fittings</strong>, I built
+        an Android app backend using <strong>Express.js</strong>, integrating{" "}
+        <strong>Google AdMob</strong> for monetization and providing real-time
+        metal price updates.
+      </p>
+
+      <p>
+        Passionate about problem-solving, I’ve solved{" "}
+        <strong>500+ DSA questions</strong> and enjoy working on{" "}
+        <strong>embedded systems and robotics</strong>. I’m also a chess
+        enthusiast and <strong>IIT Guwahati gold medalist</strong>.
       </p>
     </section>
   );
